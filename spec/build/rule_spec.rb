977274b2ece97fcc8d8bffc39401702ed82f8509
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-09-25 11:49:16 +1200

    Add spec.

--- spec/build/rule_spec.rb
+++ spec/build/rule_spec.rb
@@ -32,4 +32,22 @@ RSpec.describe Build::Rule do
 		expect(rule.applicable?(source: 'foo', destination: 'bar')).to be_truthy
 		expect(rule.applicable?(source: 'foo')).to be_falsey
 	end
+	
+	it "respects false argument" do
+		rule = Build::Rule.new("compile", "cpp")
+		
+		rule.parameter :install, default: true
+		
+		expect(
+			rule.normalize({}, binding)
+		).to be == {install: true}
+		
+		expect(
+			rule.normalize({install: true}, binding)
+		).to be == {install: true}
+		
+		expect(
+			rule.normalize({install: false}, binding)
+		).to be == {install: false}
+	end
 end
